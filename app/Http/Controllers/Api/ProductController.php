5268ab1e9c87f009dae9ff1a7de90c31da75413c
Irvan Dwi Heriawan <emailgithub_irvandwi21@gmail.com>
2026-04-26 22:17:44 +0700

    Finish CRUD API products

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Product::all());
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+        return response()->json($product);
+    }
+
+    public function show($id)
+    {
+        return response()->json(Product::find($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return response()->json($product);
+    }
+
+    public function destroy($id)
+    {
+        Product::destroy($id);
+        return response()->json(['message' => 'Deleted']);
+    }
+}
